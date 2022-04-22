7d2291bec3a9db58c3553d8a17ab9911075c9154
Vitall <temmets20@gmail.com>
2022-04-22 21:44:26 +0700

    secondACtivity

---
+++ app/src/main/java/com/example/module7/SecondActivity.kt
@@ -0,0 +1,22 @@
+package com.example.module7
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+
+class SecondActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        val textView = findViewById<TextView>(R.id.textView).apply{
+            text = "Привет, $message !"
+        }
+    }
+
+
+}
