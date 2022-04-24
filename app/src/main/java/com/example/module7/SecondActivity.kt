396d472de92d489de257d606aea51a32c0fa9be5
Vitall <temmets20@gmail.com>
2022-04-24 21:12:17 +0700

    vars

--- app/src/main/java/com/example/module7/SecondActivity.kt
+++ app/src/main/java/com/example/module7/SecondActivity.kt
@@ -3,11 +3,13 @@ package com.example.module7
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.View.OnTouchListener
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView;
 
@@ -17,32 +19,27 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-        val container = findViewById<LinearLayout>(com.example.module7.R.id.container)
-        var yDelta = 0
-        var xDelta = 0
-        val touchListener = OnTouchListener { view, event ->
-            val x = event.rawX.toInt()
-            val y = event.rawY.toInt()
-            when (event.action and MotionEvent.ACTION_MASK) {
-                MotionEvent.ACTION_DOWN -> {
-                    val lParams = view.layoutParams as LinearLayout.LayoutParams
-                    xDelta = x - lParams.leftMargin
-                    yDelta = y - lParams.topMargin
-                }
-                MotionEvent.ACTION_MOVE -> {
-                    if (x - xDelta + view.width <= container.getWidth() && y - yDelta + view.height <= container.getHeight() && x - xDelta >= 0 && y - yDelta >= 0) {
-                        val layoutParams = view.layoutParams as LinearLayout.LayoutParams
-                        layoutParams.leftMargin = x - xDelta
-                        layoutParams.topMargin = y - yDelta
-                        layoutParams.rightMargin = 0
-                        layoutParams.bottomMargin = 0
-                        view.layoutParams = layoutParams
-                    }
-                }
-            }
-            container.invalidate()
-            true
+    }
+
+    class Vars {
+        val numbersMap = mutableMapOf<String, String>()
+
+        fun puttingData(key: String, value:String ){
+            numbersMap.put(key,value)
+            println(numbersMap)
         }
     }
+    val vav:Vars = Vars()
+    fun addVariable(view: View){
+        val v = vav
+        val editNameVar = findViewById<EditText>(R.id.varName)
+        val varName = editNameVar.text.toString()
+        val editAmountVar = findViewById<EditText>(R.id.varVal)
+        val varAmount = editAmountVar.text.toString()
+        v.puttingData(varName, varAmount)
+        editNameVar.setText("")
+        editAmountVar.setText("")
+    }
+
 
 }
