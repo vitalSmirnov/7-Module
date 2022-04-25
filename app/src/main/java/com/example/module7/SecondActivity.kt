1f7f9d080af56dfd3aab61034674ba1509e47730
Vitall <temmets20@gmail.com>
2022-04-25 19:39:24 +0700

    menu1

--- app/src/main/java/com/example/module7/SecondActivity.kt
+++ app/src/main/java/com/example/module7/SecondActivity.kt
@@ -1,17 +1,18 @@
 package com.example.module7
 
 import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.View.OnTouchListener
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.TextView;
+import android.widget.TextView
+import android.widget.PopupMenu
 
 
 class SecondActivity : AppCompatActivity() {
@@ -20,6 +21,8 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
     }
+    val textView: TextView = findViewById(R.id.textView)
+    val button = findViewById<Button>(R.id.popMenuBtn)
 
     class Vars {
         val numbersMap = mutableMapOf<String, String>()
@@ -33,13 +36,22 @@ class SecondActivity : AppCompatActivity() {
     fun addVariable(view: View){
         val v = vav
         val editNameVar = findViewById<EditText>(R.id.varName)
+        val showVars = findViewById<TextView>(R.id.varView)
         val varName = editNameVar.text.toString()
         val editAmountVar = findViewById<EditText>(R.id.varVal)
         val varAmount = editAmountVar.text.toString()
         v.puttingData(varName, varAmount)
         editNameVar.setText("")
         editAmountVar.setText("")
+        showVars.setText("${v.numbersMap}")
     }
+
+    val popupMenu2 = PopupMenu(this, button)
+    popupMenu2
+
+}
+}
+
 
 
 }
