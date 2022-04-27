6e70c910519190a55a5c8e79ba320a66fbbb24cb
Vitall <temmets20@gmail.com>
2022-04-28 03:11:49 +0700

    menu&add

--- app/src/main/java/com/example/module7/SecondActivity.kt
+++ app/src/main/java/com/example/module7/SecondActivity.kt
@@ -1,18 +1,15 @@
 package com.example.module7
 
 import android.annotation.SuppressLint
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
+import android.app.AlertDialog
+import android.app.Dialog
+import android.app.PendingIntent.getActivity
+import android.content.Context
 import android.os.Bundle
+import android.support.v4.app.DialogFragment
 import android.support.v7.app.AppCompatActivity
-import android.view.MotionEvent
 import android.view.View
-import android.view.View.OnTouchListener
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.PopupMenu
+import android.widget.*
 
 
 class SecondActivity : AppCompatActivity() {
@@ -20,9 +17,36 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        val btnPopup = findViewById<Button>(R.id.btn_popup)
+        btnPopup.setOnClickListener(){
+            var popup = PopupMenu(this, btnPopup)
+            popup.inflate(R.menu.popup_menu)
+            popup.setOnMenuItemClickListener {
+                when (it.itemId) {
+                    1 -> {addVariable()
+                        true}
+                    else -> {addVariable()
+                    true}
+                }
+            }
+            popup.show()
+        }
+
     }
-    val textView: TextView = findViewById(R.id.textView)
-    val button = findViewById<Button>(R.id.popMenuBtn)
+
+    class CustomDialogFragment : DialogFragment() {
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+            val builder: AlertDialog.Builder = AlertDialog.Builder(activity)
+
+            return builder
+                .setTitle("Определить переменную")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setMessage("Для закрытия окна нажмите ОК")
+                .setPositiveButton("OK", null)
+                .setNegativeButton("Отмена", null)
+                .create()
+        }
+    }
 
     class Vars {
         val numbersMap = mutableMapOf<String, String>()
@@ -33,25 +57,27 @@ class SecondActivity : AppCompatActivity() {
         }
     }
     val vav:Vars = Vars()
-    fun addVariable(view: View){
-        val v = vav
-        val editNameVar = findViewById<EditText>(R.id.varName)
-        val showVars = findViewById<TextView>(R.id.varView)
-        val varName = editNameVar.text.toString()
-        val editAmountVar = findViewById<EditText>(R.id.varVal)
-        val varAmount = editAmountVar.text.toString()
-        v.puttingData(varName, varAmount)
-        editNameVar.setText("")
-        editAmountVar.setText("")
-        showVars.setText("${v.numbersMap}")
+    fun addVariable(){
+//        val v = vav
+//        val editNameVar = findViewById<EditText>(R.id.varName)
+//        val showVars = findViewById<TextView>(R.id.varView)
+//        val varName = editNameVar.text.toString()
+//        val editAmountVar = findViewById<EditText>(R.id.varVal)
+//        val varAmount = editAmountVar.text.toString()
+//        v.puttingData(varName, varAmount)
+//        editNameVar.setText("")
+//        editAmountVar.setText("")
+//        showVars.setText("${v.numbersMap}")
+        val mainContainer = findViewById<LinearLayout>(R.id.codeContainer)
+        val block = layoutInflater.inflate(R.layout.val_block, mainContainer, true)
     }
 
-    val popupMenu2 = PopupMenu(this, button)
-    popupMenu2
+
+    fun showDialog(view: View) {
+        val dialog = CustomDialogFragment();
+        dialog.show(supportFragmentManager, "custom");
+    }
+
 
 }
-}
 
-
-
-}
